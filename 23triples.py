f4c00c3d166eebb704e5e5a81ee518d20d241eb9
khynguyen <khynguyen@ucdavis.edu>
2025-02-06 15:30:56 -0800

    update

--- 23triples.py
+++ 23triples.py
@@ -1,6 +1,9 @@
 #23triples.py by Karen Nguyen
 
-for a in range(1, 101):
-	for b in range(1, 101): 
+limit = 101
+
+for a in range(1, limit):
+	for b in range(a, limit): 
 		c = (a**2 + b**2) ** 0.5 
 		if c % 1 == 0: print(a, b, c)
+
