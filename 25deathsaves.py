d004e44c58f74094289bdb4a5bd3b7b876e9dec2
khynguyen <khynguyen@ucdavis.edu>
2025-02-06 18:16:27 -0800

    update

--- 25deathsaves.py
+++ 25deathsaves.py
@@ -2,16 +2,28 @@
 
 import random
 
-def deathsaves():
-	die = random.randint(1, 20)
-	print(die)
-	if die == 1:  return 'double failture!'
-	elif die == 20: return 'revived!'
-	elif die < 10: return 'failure!'
-	elif die >= 10: return 'success!'
+trial = 10000
+revive = 0 
+die = 0 
+stable = 0
 
-print(deathsaves())
-print(deathsaves())
-print(deathsaves())
-print(deathsaves())
-print(deathsaves())
+for i in range(trial):
+	successes = 0 
+	failures = 0 
+	for i in range(5):
+		roll = random.randint(1, 20)
+		if roll == 1: failures += 2
+		elif roll < 10: failures += 1 
+		elif roll <= 19: successes += 1
+		else: 
+			revive += 1 
+			break
+		
+		if successes == 3: 
+			stable += 1
+			break
+		if failures >= 3: 
+			die += 1
+			break
+		
+print(stable/trial, revive/trial, die/trial)	
