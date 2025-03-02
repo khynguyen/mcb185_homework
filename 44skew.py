7dd39cc72fe16452bf3b5f7e71e2afb1a547c7f8
khynguyen <khynguyen@ucdavis.edu>
2025-03-02 15:29:54 -0800

    update

--- 44skew.py
+++ 44skew.py
@@ -1,14 +1,25 @@
 #44skew.py by Karen Nguyen
 
 import sequence 
+import sys 
+import mcb185
+	
+for defline, seq in mcb185.read_fasta(sys.argv[1]):
+	w = int(sys.argv[2])
+	s = 1
+	subseq = seq[0:w]
 
-seq = 'ACGTACGTGGGGGACGTACGTCCCCC'
+	c = subseq.count('C')
+	g = subseq.count('G')
+	print(0, sequence.gc_comp(subseq), sequence.gc_skew(subseq))
 
-w = 10 
-s = 1
-subseq = seq[0:w]
-gc_
-print(sequence.gc_comp(subseq), sequence.gc_skew(subseq))
-
-for i in range(0, len(seq)-w+1, s):
-	subseq[]
+	for i in range(0, len(seq)-w , s):
+		if   seq[i + w] == 'G': g += 1
+		elif seq[i + w] == 'C': c += 1
+		
+		if   seq[i] == 'G': g -= 1
+		elif seq[i] == 'C': c -= 1
+		
+		comp = (g + c) / w
+		skew = (g - c) / (g + c)
+		print(i + 1, g + c, comp, skew)
--- 45colorname.py
+++ 45colorname.py
@@ -1,6 +1,6 @@
 #45colorname.py by Karen Nguyen
 
-#colors_extended are formatted as (name \t hex code \t r,g,b)
+#colors file formatting: name, hex code, r/g/b
 
 import sys
 
@@ -10,19 +10,23 @@ g = int(sys.argv[3])
 b = int(sys.argv[4])
 
 value_diff = 765 #max possible difference to have, 255*3 
+color_name = 'blank'
 
 with open(colorfile, 'rt') as fp:
 	for line in fp:
-		colors = line.split()
-		color_values = colors[2].split(',')
+		colors = line.split() #split to get name and values
+		color_values = colors[2].split(',') #get just values
 		
-		R = int(color_values[0])
+		R = int(color_values[0]) 
 		G = int(color_values[1])
 		B = int(color_values[2])
 		
+		#take total difference
 		diff = abs(R - r) + abs(G - g) + abs(B - b)
+		#if difference is smaller than prev, get name from first list in line 16
 		if diff < value_diff: 
 			value_diff = diff
-			color_name = colors[0]
+			color_name = colors[0] 
+			value = colors[2] 
 
-print(color_name)
+print(color_name, value, value_diff, sep='    ')	
