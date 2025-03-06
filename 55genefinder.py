31d622a43bb8570d5c10ac88d3595213576bdb63
khynguyen <khynguyen@ucdavis.edu>
2025-03-06 00:41:24 -0800

    update

--- 55genefinder.py
+++ 55genefinder.py
@@ -1,6 +1,6 @@
 #55genefinder.py by Karen Nguyen 
 
-##should be 4337 CDS total
+##should be ~4337 CDS total
 ##zless GCF_000005845.2_ASM584v2_genomic.gff.gz | cut -f3 | grep 'CDS' | wc
 
 import sys 
@@ -19,13 +19,15 @@ for defline, seq in mcb185.read_fasta(file):
 	for i in range(start_for, len(seq)): 
 		codon_for = seq[start_for:start_for+3]	
 		
+		#if start codon, find stop codon by going by 3's
 		if codon_for == 'ATG': 	
 			for j in range(start_for, len(seq), 3):
 				codon = seq[j:j+3]
 				
+				#if stop codon, get coordinates of orf 
 				if codon == 'TAA' or codon == 'TAG' or codon == 'TGA':
 					orf = seq[start_for:j+3]
-					if len(orf) < orf_length: 
+					if len(orf) < orf_length:  #if too short, move on
 						start_for = j + 3
 						break
 						
@@ -42,13 +44,15 @@ for defline, seq in mcb185.read_fasta(file):
 	for i in range(start_rev, len(revseq)): 
 		codon_rev = revseq[start_rev:start_rev+3]	
 		
+		#if start codon, find stop codon by going by 3's
 		if codon_rev == 'ATG': 	
 			for j in range(start_rev, len(revseq), 3):
 				codon = revseq[j:j+3]
 				
+				#if stop codon, get coordinates of orf
 				if codon == 'TAA' or codon == 'TAG' or codon == 'TGA':
 					orf = revseq[start_rev:j+3]
-					if len(orf) < orf_length: 
+					if len(orf) < orf_length: #if too short, move on
 						start_rev = j + 3
 						break
 					
